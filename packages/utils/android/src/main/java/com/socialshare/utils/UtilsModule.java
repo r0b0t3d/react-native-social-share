e77614ee8b742baae628f980994263e53bb734b6
Tuan Luong <tuanluong.it@gmail.com>
2019-12-13 11:19:30 +0700

    update package android

--- packages/utils/android/src/main/java/com/socialshare/utils/UtilsModule.java
+++ packages/utils/android/src/main/java/com/socialshare/utils/UtilsModule.java
@@ -3,7 +3,8 @@ package com.socialshare.utils;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v4.content.FileProvider;
+
+import androidx.core.content.FileProvider;
 
 import com.facebook.react.bridge.Promise;
 import com.facebook.react.bridge.ReactApplicationContext;
