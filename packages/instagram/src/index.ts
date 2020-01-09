ec70678348c3b6e53c5d69e833cf7495c3cd85d9
Tuan Luong <tuanluong.it@gmail.com>
2020-01-09 16:30:54 +0700

    share photo instagram

--- packages/instagram/src/index.ts
+++ packages/instagram/src/index.ts
@@ -3,15 +3,16 @@ import { NativeModules, Platform, Linking } from 'react-native';
 import ShareUtils, { SocialError } from '@react-native-social-share/utils';
 const { InstagramShare } = NativeModules;
 
+const appIdentifier = Platform.select({
+  android: 'com.instagram.android',
+  ios: 'instagram://',
+});
+
 async function shareLink(link: string, description: string) {
   throw new Error('Not available');
 }
 
 async function shareVideo(videoUri: string) {
-  const appIdentifier = Platform.select({
-    android: 'com.instagram.android',
-    ios: 'instagram://',
-  });
   const isInstagramInstalled = await ShareUtils.isAppInstalled(appIdentifier);
   if (!isInstagramInstalled) {
     throw new SocialError('APP_NOT_INSTALLED', 'Instagram must be installed');
@@ -24,7 +25,21 @@ async function shareVideo(videoUri: string) {
   }
 }
 
+async function sharePhoto(options: any) {
+  const isInstagramInstalled = await ShareUtils.isAppInstalled(appIdentifier);
+  if (!isInstagramInstalled) {
+    throw new SocialError('APP_NOT_INSTALLED', 'Instagram must be installed');
+  }
+
+  if (Platform.OS === 'ios') {
+    return Linking.openURL(`instagram://library?AssetPath=${options.localFile}`);
+  } else if (Platform.OS === 'android') {
+    return InstagramShare.shareVideo(options.localFile);
+  }
+}
+
 export default {
   shareLink,
   shareVideo,
+  sharePhoto
 };
